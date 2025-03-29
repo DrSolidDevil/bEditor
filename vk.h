67a5d3a65b8ef5d14f84f045e0bb2d7293046477
DrSolidDevil <51828495+DrSolidDevil@users.noreply.github.com>
2025-03-30 00:59:10 +0100

    Create vk.h

---
+++ vk.h
@@ -0,0 +1,67 @@
+#ifndef VK_LETTER_A
+#define VK_LETTER_A 0x41
+#endif
+
+#ifndef VK_LETTER_N
+#define VK_LETTER_N 0x4E
+#endif
+
+#ifndef VK_LETTER_O
+#define VK_LETTER_O 0x4F 
+#endif
+
+#ifndef VK_LETTER_Q
+#define VK_LETTER_Q 0x51
+#endif
+
+#ifndef VK_LETTER_S
+#define VK_LETTER_S 0x53
+#endif
+
+#ifndef VK_LETTER_Y
+#define VK_LETTER_Y 0x59
+#endif
+
+#ifndef VK_LETTER_Z
+#define VK_LETTER_Z 0x5A
+#endif
+
+#ifndef VK_NUMBER_0
+#define VK_NUMBER_0 0x30
+#endif
+
+#ifndef VK_NUMBER_1
+#define VK_NUMBER_1 0x31
+#endif
+
+#ifndef VK_NUMBER_2
+#define VK_NUMBER_2 0x32
+#endif
+
+#ifndef VK_NUMBER_3
+#define VK_NUMBER_3 0x33
+#endif
+
+#ifndef VK_NUMBER_4
+#define VK_NUMBER_4 0x34
+#endif
+
+#ifndef VK_NUMBER_5
+#define VK_NUMBER_5 0x35
+#endif
+
+#ifndef VK_NUMBER_6
+#define VK_NUMBER_6 0x36
+#endif
+
+#ifndef VK_NUMBER_7
+#define VK_NUMBER_7 0x37
+#endif
+
+#ifndef VK_NUMBER_8
+#define VK_NUMBER_8 0x38
+#endif
+
+#ifndef VK_NUMBER_9
+#define VK_NUMBER_9 0x39
+#endif
